be73b93dc41772886a0953935f7fb5aff4e9652c
susiewebb <susie.webb@hearst.com>
2025-11-25 13:21:58 -0500

    Update current_update.R

--- current_update.R
+++ current_update.R
@@ -31,7 +31,7 @@ parse_table_rows <- function(page) {
 
   
 ##Working on the Airline updates
-airports <- c('Delta Air Lines|Southwest|American Airlines|United')
+airports <- '(^Delta Air Lines$)|(^Southwest$)|(^American Airlines$)|(^United Airlines$)'
 
 airportNow_html <- read_html('https://www.flightaware.com/ajax/airport/cancelled_count.rvt?type=airline&timePeriod=today&airportFilter=')
 airportYest_html <- read_html('https://www.flightaware.com/ajax/airport/cancelled_count.rvt?type=airline&timePeriod=yesterday&airportFilter=')
